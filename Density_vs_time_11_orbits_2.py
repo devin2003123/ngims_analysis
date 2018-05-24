8cdbea83f139ba6ee1001758b189f91856a0f927
Devin Bush <devin2003123@gmail.com>
2018-05-23 22:52:12 -0400

    added python file

---
+++ Density_vs_time_11_orbits_2.py
@@ -0,0 +1,134 @@
+import pandas as pd
+from numpy import arange, exp, mean
+import numpy as np
+from datetime import datetime, timedelta
+from matplotlib import pyplot as plt
+from glob import glob
+import pdb
+import os
+from sys import exit
+
+while -1:
+    print "Enter Altitude"
+    UserAlt = input()
+
+    if UserAlt < 2.5:
+        print "Invalid Altitude (Please select altitude >2.5km)"
+        continue
+    else:
+        break
+
+uLimit = UserAlt + 2.5
+lLimit = UserAlt - 2.5
+
+
+def read_one_file(file):
+
+    f = open(file,'r')
+    data = []
+    next(f) #skip the file header line
+
+    altlist = []
+    # split across line, only keep data of interest
+
+    for line in f:
+        temp = line.split(',')
+        datestring = temp[0]
+        year = int(datestring[0:4])
+        month = int(datestring[5:7])
+        day = int(datestring[8:10])
+        hour = int(datestring[11:13])
+        minute = int(datestring[14:16])
+        second = int(datestring[17:19])
+        date = datetime(year,month,day,hour,minute,second)
+
+        species = temp[13]
+        alt = float(temp[7])
+        lon = float(temp[9])
+        lat = float(temp[10])
+        orbit = float(temp[5])
+
+        try:
+            den = float(temp[15])
+            altlist.append(alt)
+
+            if alt <= uLimit and alt >= lLimit: # for this exercise, we only consider data below 300km
+                tempdict = {'date': date, 'alt': alt, 'den': den, 'species': species, 'longitude': lon, 'latitude': lat, 'orbit': orbit}
+                if alt <= altlist[-1]: # only want inbound leg of orbit
+                    data.append(tempdict)
+        except:
+            pass
+    return data
+
+def read_ngims(mylist):
+    alldata = []
+    for file in mylist:
+        temp_data = read_one_file(file)
+        alldata.append(temp_data)
+
+    return alldata
+
+def date_range(startdate,enddate):
+    filelist = glob('C:/Users/devinrb/Documents/2016_2017 School Year/Thesis/NGIMSData/NGIMS_327_508/*cs*.csv') #grab all MAVEN data from downloaded files using glob
+    if len(filelist) < 1:
+        print "No MAVEN files found!"
+        print "Exiting!"
+        exit(1)
+    mylist = []
+    for file in filelist: # grab dates from file names
+        fileyear = int(file[-27:-23])
+        filemonth = int(file[-23:-21])
+        fileday = int(file[-21:-19])
+        filedate = datetime(fileyear,filemonth,fileday,0,0,0)
+
+        if startdate <= filedate <= enddate:
+            mylist.append(file)
+    return mylist
+
+startdate = datetime(2016,3,25)
+enddate = datetime(2016,5,10)
+#dates = pd.date_range(startdate,enddate,freq='2min')
+mylist = date_range(startdate,enddate)
+all_ngims = read_ngims(mylist)
+all_ngims = [item for sublist in all_ngims for item in sublist]
+
+ngims_df = pd.DataFrame.from_records(data = all_ngims, index = ('orbit'))
+
+print "Select Species"
+print "1. Argon (Ar)"
+print "2. Carbon Dioxide (CO2)"
+print "3. Nitrogen Gas (N2)"
+print "4. Helium (He)"
+print "5. Carbon Monoxide (CO)"
+print "6. Oxygen (O)"
+
+#prompt user for desired molecule symbol
+while 1:
+    choice = raw_input()
+    mydf = ngims_df[ngims_df['species'] == '%s' %(choice)]
+    if mydf.empty:
+        print "Error: No data for specified molecule -- Try again"
+        continue
+    else:
+        break
+
+
+
+#plot the graph with chosen element
+
+
+mydf['date'] = np.int64(mydf['date'])
+mydf = mydf.groupby(mydf.index).mean()
+df_den_copy = mydf['den'].copy()
+df_date_copy = mydf['date'].copy()
+rolling_avg_den = df_den_copy.rolling(window = 11, center = True).mean()
+rolling_avg_date = df_date_copy.rolling(window = 11, center = True).mean()
+x_axis = rolling_avg_date.dropna()
+x_axis = pd.DatetimeIndex(x_axis)
+y_axis = rolling_avg_den.dropna()
+plt.plot(x_axis,y_axis, 'bo')
+plt.gcf().autofmt_xdate()
+plt.xlabel('Time',fontsize=14)
+plt.ylabel('Density ($Molecules/cm^3$)')
+plt.title('%i km'%(UserAlt))
+plt.show()
